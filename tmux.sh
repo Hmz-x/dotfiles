d66e158db2986fab1efb5ad917df0188720312bd
HamzaKerem <hamzamusic34@gmail.com>
2021-12-07 20:25:31 +0300

    Made changes to tmux

--- tmux.sh
+++ tmux.sh
@@ -1,29 +1,35 @@
 #!/bin/sh
 
+# Constants
+SESH="default" # default session
+MUSIC_PLAYER="cmus"
+FILE_MAN="lf"
+
 { [ -n "$1" ] && dir="$1"; } || dir="$HOME"
 { [ -n "$EDITOR" ] && editor="$EDITOR"; } || editor="vim"
 
 wbi=0 # window base index
 pbi=0 # pane base index
-sesh="default" # default session
-music_player="cmus"
 
 # If session exists, attach to it and finally quit.
-tmux has-session -t "$sesh" && tmux attach -t "$sesh" && exit 0
+tmux has-session -t "$SESH" && tmux attach -t "$SESH" && exit 0
 
 # If not, create new session in the background.
-# Name the first window "editor", name the second window "music"
-# Create a new horizontal pane.
-tmux new-session -s "$sesh" -n editor -d
-tmux new-window -t "$sesh" -n music 
-tmux split-window -v -p 10 -t "$sesh":$wbi
+tmux new-session -s "$SESH" -n editor -d # Name the first window "editor"
+# Name the second "manager", the third "music"
+tmux new-window -t "$SESH" -n manager 
+tmux new-window -t "$SESH" -n music 
+# Create a new horizontal pane for the first window.
+tmux split-window -v -p 20 -t "$SESH":$wbi
 
-# To all panes, cd into $dir and then clear.
+# For all panes, cd into $dir and then clear.
+tmux send-keys -t "$SESH":$wbi.$pbi "cd $dir; clear" C-m
+tmux send-keys -t "$SESH":$wbi.$((pbi+1)) "cd $dir; clear" C-m
+# In "manager" window, launch file manager
+tmux send-keys -t "$SESH":$((wbi+1)) "cd $dir; clear; $FILE_MAN" C-m
 # In "music" window, launch music player
-tmux send-keys -t "$sesh":$wbi.$pbi "cd $dir; clear" C-m
-tmux send-keys -t "$sesh":$wbi.$((pbi+1)) "cd $dir; clear" C-m
-tmux send-keys -t "$sesh":$((wbi+1)) "cd $dir; clear; $music_player" C-m
+tmux send-keys -t "$SESH":$((wbi+2)) "cd $dir; clear; $MUSIC_PLAYER" C-m
 
 # Select the starting window and attach session
-tmux select-window -t "$sesh":$((wbi+1))
-tmux attach -t "$sesh"
+tmux select-window -t "$SESH":$((wbi+2))
+tmux attach -t "$SESH"
