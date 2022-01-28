c660a73c37687ababe34aa1222bc5e30b6461719
HamzaKerem <hamzamusic34@gmail.com>
2022-01-28 16:55:27 +0300

    Updated ms2pdf for eqn

--- herbstluftwm_scripts/ms2pdf.sh
+++ herbstluftwm_scripts/ms2pdf.sh
@@ -1,10 +1,32 @@
 #!/bin/sh
 
-# Convert given ms file to pdf
+# Convert given ms file to pdf. Written by Hamza Kerem Mumcu
 
+# Program data
+pdf_prg="zathura" # Program to read pdf files with
 out_ext="pdf"
+filename="$1"
+apply_eqn_bool="false"
 
-new_file="$(chext.sh "$1" "$out_ext")"
-groff -ms "$1" -T "$out_ext" > "$new_file"
+apply_eqn(){
+	# If strings .EQ and .EN exist in file, set apply_eqn_bool to true
+	grep -wq "\.EQ" "$filename" && grep -wq "\.EN" "$filename" &&
+	apply_eqn_bool="true"
+}
 
-exec zathura "$new_file"
+compile(){
+	new_file="$(chext.sh "$filename" "$out_ext")"
+
+	# Compile with eqn flags and return
+	[ "X${apply_eqn_bool}" = "Xtrue" ] &&
+	groff -ms -e "$filename" -T "$out_ext" > "$new_file" && return
+
+	groff -ms "$filename" -T "$out_ext" > "$new_file"
+}
+
+apply_eqn
+
+compile
+
+# Read PDF
+exec "$pdf_prg" "$new_file"
