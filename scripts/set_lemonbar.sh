6f4975d7b40a82a31639fcf91a3bc9bf606e05cc
HamzaKerem <hamzamusic34@gmail.com>
2022-02-22 10:00:23 +0300

    set_lemonbar.sh updating

--- scripts/set_lemonbar.sh
+++ scripts/set_lemonbar.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Script constants
+DEFAULT_SLEEP_TIME="1"
+
 get_acpi_info()
 {
 	charging_percentage="$(acpi | cut -d ' ' -f 4 | cut -b -3)"
@@ -36,11 +39,10 @@ get_mpc_info()
 	[ "$repeat_status" = "on" ] && repeat_status_letter="R"
 
 	# If --ttf-font-awesome is passed, echo icon strings too
-	if [ "X$1" = 'X--ttf-font-awesome' ]; then
+	if  [ "$ttf_fa_bool" = "true" ]; then
 		state_status="$(mpc status '%state%')"
 		[ "$state_status" = "playing" ] && state_icon="\\uf001"
 		[ "$state_status" = "paused" ] && state_icon="\\uf05e"
-
 	fi	
 
 	echo "mpc volume:${vol} ${song} (${songpos}/${pl_length}) " \
@@ -48,19 +50,56 @@ get_mpc_info()
 		"${state_icon}"
 }
 
-get_hlwm_info()
+get_date_info()
 {
-	#printf -- "%s\n" "$hlwm_tag_envvar"
-	:
+	date '+%c'
 }
 
-while true; do
-	sleep_time='0.2'
-	sleep $sleep_time
+call_later()
+{
+	echo "$1"
+}
 
-	lstring="%{l} $(get_hlwm_info) | $(get_nmcli_info)"
-	cstring="%{c} PA volume: $(pamixer --get-volume) | $(get_mpc_info "$1")"
-	rstring="%{r} $(get_acpi_info) | $(date '+%c')"
-	
-	echo -e "${lstring} ${cstring} ${rstring}"
-done
+parse_options()
+{
+	# true if --ttf-font-awesome pos-param is passed
+	ttf_fa_bool="false"
+	# Result to default sleep_time if --sleep-time SLEEP_TIME isn't passed
+	sleep_time="$DEFAULT_SLEEP_TIME"
+
+	while [ "$#" -gt 0 ]; do
+		case "$1" in
+			'--ttf-font-awesome') ttf_fa_bool="true";;	
+			'--sleep-time') 
+				if [ -z "$2" ]; then
+					printf -- "%s\n" "Non-fatal: sleep time not given." 2>&1
+				else
+					sleep_time="$2";
+					shift
+				fi;;
+			*) output="$("$1" 2> /dev/null || printf -- "%s\n" "INVALID_INPUT")"; 
+				[ "X$output" != "XINVALID_INPUT" ] && call_later "$1";;
+		esac
+	shift	
+	done
+}
+
+
+# Parse pos-param options
+parse_options "$@"
+
+lemonbar_loop()
+{
+	while true; do
+		echo "sleep: $sleep_time"
+		sleep $sleep_time
+
+		lstring="%{l} $(get_nmcli_info)"
+		cstring="%{c} PA volume: $(pamixer --get-volume) | $(get_mpc_info)"
+		rstring="%{r} $(get_acpi_info) | $(get_date_info)"
+		
+		echo -e "${lstring} ${cstring} ${rstring}"
+	done
+}
+
+lemonbar_loop "$@"
