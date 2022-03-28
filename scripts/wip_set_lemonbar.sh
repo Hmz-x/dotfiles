dc0fc074f32d0937616839a0455df8f3d3e54201
HamzaKerem <hamzamusic34@gmail.com>
2022-03-28 16:41:31 +0300

    Working on some more lemonbar stuff

--- scripts/wip_set_lemonbar.sh
+++ scripts/wip_set_lemonbar.sh
@@ -2,11 +2,34 @@
  
 # Script constants
 DEFAULT_SLEEP_TIME="1"
+SEPERATOR_STRING="|"
+
+return_space_count()
+{
+	local_space_count=$(echo "$1" | grep -o '[[:space:]]' | wc -l)
+	echo $local_space_count
+}
+
+return_strlen()
+{
+	local_strlen=$(printf "%s" "$1" | wc -m)
+	echo $local_strlen
+}
 
 get_text()
 {
-	input="TESTING"
-	echo "$input" | cut -b -$get_text_char_count
+	input="$1"
+
+	# Print spaces
+	#for i in $(seq 0 $((init_ch_count-var_ch_count))); do
+		#printf " "
+	#done
+	
+	# Print characters
+	#for i in $(seq $var_ch_count $init_ch_count); do
+		#printf "$input" | cut -c $i
+	#done
+	echo "XXX $input XXX" # | cut -b -$var_ch_count
 }
 
 get_cpu_info()
@@ -18,7 +41,7 @@ get_cpu_info()
 	echo "usr: ${usr_data} sys: ${sys_data}"
 }
 
-get_window_titles()
+return_window_titles()
 {
 	recieved_window_titles=""
 	temp_window_titles_file="$(mktemp)"
@@ -28,7 +51,7 @@ get_window_titles()
 	
 	while read -r line; do
 		# Get only last string in the entire line
-		space_count="$(echo "$line" | grep -o '[[:space:]]' | wc -l)"
+		space_count="$(return_space_count "$line")"
 		if ((space_count==0)); then
 			single_window_title="$line"
 		else
@@ -45,7 +68,7 @@ get_hlwm_info()
 {
 	# Local constants
 	SPACE_COUNT=20
-	TOTAL_MONITOR_COUNT=4
+	TOTAL_TAG_COUNT=4
 	REGULAR_TAG_COLOR="#000000"
 	CURRENT_TAG_COLOR="#FFFFFF"
 	REGULAR_TAG_BG_COLOR="#EEFF7C"
@@ -54,7 +77,7 @@ get_hlwm_info()
 	current_tag="$(herbstclient list_monitors | cut -d '"' -f 2)"
 	
 	# Print all tag boxes each containing tag number and window titles
-	for tag in $(seq 1 $TOTAL_MONITOR_COUNT); do
+	for tag in $(seq 1 $TOTAL_TAG_COUNT); do
 
 		# Print color formatting
 		if [ "$current_tag" = "$tag" ]; then
@@ -70,10 +93,10 @@ get_hlwm_info()
 		list_clients_output="$(herbstclient list_clients --tag="$tag" --title)"
 		if [ -n "$list_clients_output" ]; then
 			# Parse through the entire data and only get the program name
-			window_titles="$(get_window_titles)"
+			window_titles="$(return_window_titles)"
 
 			# Calculate number of spaces needed for the rest of the "tag box"
-			window_titles_strlen=$(printf "%s" "$window_titles" | wc -c)
+			window_titles_strlen=$(return_strlen "$window_titles")
 			spaces_left=$((SPACE_COUNT-window_titles_strlen-1))
 
 			# Print window titles of the programs only
@@ -148,36 +171,36 @@ get_pulseaudio_info()
 
 call_later()
 {
+	# Write passed_dir and passed_func to temp_loop_file, and passed_arg if exists
+
 	# Passed aligment direction
 	passed_dir="%{$1}"
 	# Passed executable function
 	passed_func="$2"
+	# Passed argument for function
+	passed_arg="$3"
 
 	# Create temp file if non-existent
 	if [ ! -f "$temp_loop_file" ]; then
 		temp_loop_file="$(mktemp)" || 
 		{ printf -- "%s\n" "Fatal: Couldn't create temporary file. Exitting" 2>&1; exit 1; }
-		echo "$temp_loop_file"
 	fi
 	
-	# Write alignment_direction and executable_function to temp_loop_file
-
-	# If alignment_direction is same as the previous passed_func's direction, 
-	# don't write alignment_direction
-	#echo "$temp_loop_file"
-	#echo "function, dir: $passed_func, $passed_dir"
 	line_count=$(wc -l "$temp_loop_file" | cut -d ' ' -f 1)
 
-	if ((line_count>0)) && \
+	# If alignment_direction is same as the previous passed_func's direction, 
+	# don't write passed_dir.
+	if ((line_count>0)) &&
 	awk "NR==$line_count" "$temp_loop_file" | grep "$passed_dir" -q; then
-		#printf -- " %s" "$passed_func" 
-		printf -- " %s" "$passed_func" >> "$temp_loop_file"
+		printf -- "%s %s\n" "$passed_func" "$passed_arg" >> "$temp_loop_file"
+		sed -i "${line_count}N;s/\n/ /" "$temp_loop_file" 
+	# If not, just write the new passed_dir and passed_func to file
 	else
-		# Write alignment_direction and executable_function
-		#printf -- "%s %s\n" "$passed_dir" "$passed_func" 
-		printf -- "%s %s\n" "$passed_dir" "$passed_func" >> "$temp_loop_file"
+		printf -- "In func: %s %s\n" "$passed_dir" "$passed_func"
+		printf -- "%s %s" "$passed_dir" "$passed_func" >> "$temp_loop_file"
+		[ -n "$passed_arg" ] && printf -- " %s" "$passed_arg" >> "$temp_loop_file"
+		printf "\n" >> "$temp_loop_file"
 	fi
-	#echo -e "\n\n\n"
 }
 
 parse_options()
@@ -194,15 +217,19 @@ parse_options()
 	while [ "$#" -gt 0 ]; do
 		case "$1" in
 			'--ttf-font-awesome') ttf_fa_bool="true";;	
-			'--sleep-time') [ -n "$2" ] && sleep_time="$2";;
+			'--sleep-time') [ -n "$2" ] && sleep_time="$2" && shift;;
+
 			'-l') alignment_direction="l";;
 			'-r') alignment_direction="r";;
 			'-c') alignment_direction="c";;
+
 			# Special function
-			'get_text') get_text_char_count=7 &&
-			call_later "$alignment_direction" "get_text";;
+			'get_text') [ -n "$2" ] && init_ch_count=$(return_strlen "$2") &&
+				var_ch_count=1 && call_later "$alignment_direction" "get_text" "$2" &&
+				shift;;
+
 			# If given function executes without any errors, call it later 
-			*)	"$1" &> /dev/null && call_later "$alignment_direction" "$1";;
+			*) "$1" &> /dev/null && call_later "$alignment_direction" "$1";;
 		esac
 	shift	
 	done
@@ -210,20 +237,56 @@ parse_options()
 
 lemonbar_loop()
 {
+	echo "temp file: $temp_loop_file"
+	# Read lines to output to lemonbar from temp_loop_file
 	while true; do
-		# Read string to output to lemonbar from temp_loop_file
 		lemonbar_str=""
 
-		#while read line; do
-			#echo "$line"
-		
-		while read dir_in func_in; do
-			lemonbar_str="${lemonbar_str} ${dir_in} $("$func_in")"	
-			if [ "$func_in" = "get_text" ]; then
-				((--get_text_char_count))
-				((get_text_char_count<1)) && get_text_char_count=7
-			fi
+		while read line; do
+			# Get direction first
+			lemonbar_str="$lemonbar_str $(echo "$line" | cut -d ' ' -f 1)"
+			
+			# Then, get the rest of the input functions given and add the input
+			# to lemonbar_str
+			space_count=$(return_space_count "$line")
+			for i in $(seq 2 $((space_count+1))); do
+				[ "$skip_func_in_bool" = "true" ] && continue
+
+				skip_func_in_bool="false"
+				func_to_exec="$(echo "$line" | cut -d ' ' -f $i)"
+
+				#func_to_exec="$(echo "$line" | cut -d ' ' -f $i)"
+				echo "func: $func_to_exec"
+				#((i!=2)) && prev_func="$(echo "$line" | cut -d ' ' -f $((i-1)))" &&
+				#	echo "prev func: $func_to_exec"
+
+				if [ "$func_to_exec" = "get_text" ]; then
+					skip_func_in_bool="true"
+					get_text_arg="$(echo "$line" | cut -d ' ' -f $((i+1)))"
+					echo "arg: $get_text_arg"
+					func_output="$("$func_to_exec" "$get_text_arg")"
+
+					((++var_ch_count))
+					((var_ch_count>init_ch_count)) &&
+						var_ch_count=1
+				else
+					func_output="$("$func_to_exec")"
+				fi
+
+				lemonbar_str="$lemonbar_str $func_output"
+
+				# If not the last input function, add a seperator string to lemonbar_str
+				((i<space_count+1)) && lemonbar_str="$lemonbar_str $SEPERATOR_STRING"
+			done	
 		done < "$temp_loop_file"
+		
+		#while read dir_in func_in; do
+			#lemonbar_str="${lemonbar_str} ${dir_in} $("$func_in")"	
+			#if [ "$func_in" = "get_text" ]; then
+				#((--ch_count))
+				#((ch_count<1)) && ch_count=7
+			#fi
+		#done < "$temp_loop_file"
 		
 		# Output to lemonbar
 		echo -e "$lemonbar_str"
