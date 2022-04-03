5f386f48051e4a50d1810d00ad43906277cadb63
Hmz-x <hamzamusic34@gmail.com>
2022-04-03 19:10:54 +0300

    Cleanup

--- scripts/chext.sh
+++ scripts/chext.sh
@@ -1,31 +1,12 @@
 #!/bin/sh
 
-# Print passed filename, "$1", with its extension removed.
-# Display the filename with its new extension, "$2", instead.
+[ -z "$1" ] && exit 1
 
-file="$1"
-[ -z "$file" ] && printf "No filename given. Returning.\n" && return 1
+dot_count="$(echo "$1" | grep -Fo '.' | wc -l)"
+without_ext="$(echo "$1" | cut -d '.' -f -$dot_count)"
 
-strlen="${#file}"
-dot_index=-1
-i=$((strlen-1))
-
-# Get dot index
-while [ "$i" -gt -1 ]; do
-char="$(printf "%s" "$file" | cut -b $((i+1)))"
-[ "$char" = '.' ] && dot_index="$i" && break
-i=$((i-1))
-done
-
-[ "$dot_index" -eq -1 ] && printf "No extension was detected. Returning.\n" && return 1
-
-i=0
-# Display filename without extension
-while [ "$i" -lt "$dot_index" ]; do
-char="$(printf "%s" "$file" | cut -b $((i+1)))"
-printf "%s" "$char"
-i=$((i+1))
-done
-
-# Add on new extension	
-[ -n "$2" ] && printf ".%s" "$2"
+if [ -n "$2" ]; then
+	echo "${without_ext}.${2}"
+else
+	echo "$without_ext"
+fi
--- scripts/pshift.sh
+++ scripts/pshift.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-[ -z "$1" ] && echo "No filename passed. Exitting." && exit 1
-[ -z "$2" ] && echo "No new filename passed. Exitting." && exit 1
-[ -z "$3" ] && echo "No pitch value passed. Exitting." && exit 1
-[ -z "$4" ] && echo "No range value passed. Exitting." && exit 1
-
-mv -v "$1" "$2"
-pval="$3"
-range=$(($4-1))
-
-for i in $(seq 0 $range); do
-	outfile="out${i}-${2}"
-	echo "${2} to ${outfile}: $pval"
-	sbsms "$2" "$outfile" 1 1 $pval $pval
-
-	((pval > 0)) && pval=$((pval+1))
-	((pval < 0)) && pval=$((pval-1))
-done
+if [ "$2" = "stdout" ]; then
+	soundstretch "$1" "$2" -r=$(($3*5))% | mpv -
+else
+	soundstretch "$1" "$2" -r=$(($3*5))%
+fi
