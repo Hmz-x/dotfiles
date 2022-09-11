c0cf648ae5ec6ea3c1186732478a6c6490907a45
Hmz-x <hamzamusic34@gmail.com>
2022-09-11 17:42:48 -0400

    quick fix

--- dotfiles-install.sh
+++ dotfiles-install.sh
@@ -9,47 +9,48 @@ user="$1"
 
 # Program constant
 chmod_val=755
+dotfiles_dir="/home/${user}/.local/dotfiles"
 
 # Create directories
 while read -r line; do
 	dir="/home/${user}/${line}"
 	[ ! -d "$dir" ] && install -d --owner="$user" --group="$user" --mode=$chmod_val "$dir"
-done < ./directories.txt
+done < "$dotfiles_dir/directories.txt"
 
 # Bash stuff 
 install --compare -D --owner="$user" --group="$user" --mode=$chmod_val \
-	./bash/.bashrc "/home/${user}/"
+	"$dotfiles_dir/bash/.bashrc" "/home/${user}/"
 install --compare -D --owner=root --group=root --mode=700 \
-	./bash/.bashrc /root/
+	"$dotfiles_dir/bash/.bashrc" /root/
 install --compare -D --owner="$user" --group="$user" --mode=$chmod_val \
-	./bash/.bash_profile "/home/${user}/"
+	"$dotfiles_dir/bash/.bash_profile" "/home/${user}/"
 
 # Vim stuff
 install --compare -D --owner="$user" --group="$user" --mode=$chmod_val \
-	./vim/.vimrc "/home/${user}/"
+	"$dotfiles_dir/vim/.vimrc" "/home/${user}/"
 
 # X stuff
 install --compare -D --owner="$user" --group="$user" --mode=$chmod_val \
-	./X/.xinitrc "/home/${user}/"
+	"$dotfiles_dir/X/.xinitrc" "/home/${user}/"
 install --compare -D --owner="$user" --group="$user" --mode=$chmod_val \
-	./X/.Xresources "/home/${user}/"
+	"$dotfiles_dir/X/.Xresources" "/home/${user}/"
 
 # WM, System, & Misc stuff
 install --compare -D --owner="$user" --group="$user" --mode=$chmod_val \
-	./WM/* "/home/${user}/.local/bin/WM/"
+	"$dotfiles_dir/WM/"* "/home/${user}/.local/bin/WM/"
 install --compare -D --owner="$user" --group="$user" --mode=$chmod_val \
-	./misc/* "/home/${user}/.local/bin/misc/"
+	"$dotfiles_dir/misc/"* "/home/${user}/.local/bin/misc/"
 install --compare -D --owner="$user" --group="$user" --mode=$chmod_val \
-	./system/* "/home/${user}/.local/bin/system/"
+	"$dotfiles_dir/system/"* "/home/${user}/.local/bin/system/"
 
 # Etc stuff
 install --compare -D --owner=root --group=root --mode=644 \
-	./etc/* /etc/
+	"$dotfiles_dir/etc/"* /etc/
 
 # Herbstluftwm stuff
 install --compare -D --owner="$user" --group="$user" --mode=$chmod_val \
-	./herbstluftwm/autostart "/home/${user}/.config/herbstluftwm/"
+	"$dotfiles_dir/herbstluftwm/autostart" "/home/${user}/.config/herbstluftwm/"
 
 # mpd stuff
 install --compare -D --owner="$user" --group="$user" --mode=$chmod_val \
-	./mpd/mpd.conf "/home/${user}/.config/mpd/"
+	"$dotfiles_dir/mpd/mpd.conf" "/home/${user}/.config/mpd/"
