4024805e08dd53dbe0ef94776f50c96831d0c76d
Hmz-x <hamzamusic34@gmail.com>
2023-03-08 01:29:30 -0500

    updated check_battery.sh

--- misc/check_battery.sh
+++ misc/check_battery.sh
@@ -1,24 +1,39 @@
 #!/bin/sh
 
+MED_LOW_BATTERY_PER=20
 LOW_BATTERY_PER=11
 VERY_LOW_BATTERY_PER=8
 
-battery_per="$(acpi -b | grep -Ewo '([[:digit:]]){1,3}%' | grep -Ewo '([[:digit:]]){1,3}')"
+sound_beep()
+{
+	mpc pause
+	pamixer --unmute
+	abeep -f 460 -l 300 -r 3 -d $1
+}
 
-notify-send.sh "Battery: ${battery_per}%"
-((battery_per<LOW_BATTERY_PER)) && notify-send.sh "Battery At Critical Level" && 
-	abeep -f 460 -l 300 -r 3 -d 500
+check_battery()
+{
+	notify-send.sh "Battery: ${battery_per}%"
 
-if acpi -b | grep -q "Discharging"; then
+	if ((battery_per<MED_LOW_BATTERY_PER)); then
+		notify-send.sh "Battery At Low Level"
+		sound_beep 800
+	fi
+
+	if ((battery_per<LOW_BATTERY_PER)); then
+		notify-send.sh "Battery At Critical Level"
+		sound_beep 400
+	fi
+
 	if ((battery_per<VERY_LOW_BATTERY_PER)); then 
 		notify-send.sh "Battery At Extremely Critical Level" 
-		abeep -f 460 -l 300 -r 3 -d 500
-		sleep 20
-
-		if ((battery_per<VERY_LOW_BATTERY_PER)); then 
-			notify-send.sh "Shutting Down" --action="Cancel":"pkill $0" 
-			abeep -f 460 -l 300 -r 3 -d 500
-			sleep 10 && sudo poweroff now
-		fi
+		sound_beep 200
 	fi
-fi
+
+	sleep 20
+	battery_per=$(acpi -b | grep -Ewo '([[:digit:]]){1,3}%' | grep -Ewo '([[:digit:]]){1,3}')
+	((battery_per<MED_LOW_BATTERY_PER)) && echo check && check_battery
+}
+
+battery_per=$(acpi -b | grep -Ewo '([[:digit:]]){1,3}%' | grep -Ewo '([[:digit:]]){1,3}')
+check_battery
