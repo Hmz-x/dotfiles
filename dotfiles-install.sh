ff1f444d018af0be9400f77ec26c32d638b3092a
Hmz-x <hamzamusic34@gmail.com>
2024-04-09 19:39:42 -0400

    woah

--- dotfiles-install.sh
+++ dotfiles-install.sh
@@ -88,7 +88,7 @@ install --compare -D --owner="$user" --group="$user" --mode=$chmod_val \
 	"$dotfiles_dir/starship/starship.toml" "$HOME/.config/"
 
 # pacman
-[ "$cpu_arch" = "x86_64" ] && pacman_file="$dotfiles_dir/pacman_file/arch-x64_pacman.conf"
-[ "$cpu_arch" = "aarch64" ] && pacman_file="$dotfiles_dir/pacman_file/arch-arm_pacman.conf"
+[ "$cpu_arch" = "x86_64" ] && pacman_file="$dotfiles_dir/pacman/arch-x64_pacman.conf"
+[ "$cpu_arch" = "aarch64" ] && pacman_file="$dotfiles_dir/pacman/arch-arm_pacman.conf"
 install --compare -D --owner=root --group=root --mode=644 \
 	"$pacman_file" "/etc/pacman.conf"
