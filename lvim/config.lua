1a0edf104d15a4d7c68e6e088fe662d0074802db
Hmz-x <hamzamusic34@gmail.com>
2024-07-10 17:25:22 +0300

    lvim + tmux update

--- lvim/config.lua
+++ lvim/config.lua
@@ -1,6 +1,19 @@
--- Custom keybinding to insert '#' at the beginning of the first word and move down a line
-lvim.keys.normal_mode["<C-c>"] = ":lua InsertHashAndMoveDown()<CR>"
+-- Function to go to the front of the line, remove the first character, and move down a line
+function RemoveFirstCharAndMoveDown()
+  -- Go to the front of the line
+  vim.cmd("normal! ^")
 
+  -- Remove the first character
+  vim.cmd("normal! x")
+
+  -- Move down one line
+  vim.cmd("normal! j")
+end
+
+-- Set keybinding for Ctrl-x to call the RemoveFirstCharAndMoveDown function
+vim.api.nvim_set_keymap('n', '<C-x>', ':lua RemoveFirstCharAndMoveDown()<CR>', { noremap = true, silent = true })
+
+-- Function to insert '#' at the beginning of the line and move down a line
 function InsertHashAndMoveDown()
   -- Get the current cursor position
   local line_number = vim.fn.line(".")
@@ -13,14 +26,17 @@ function InsertHashAndMoveDown()
   vim.cmd("normal! j")
 end
 
+-- Set keybinding for Ctrl-c to call the InsertHashAndMoveDown function
+vim.api.nvim_set_keymap('n', '<C-c>', ':lua InsertHashAndMoveDown()<CR>', { noremap = true, silent = true })
+
 -- Use black hole register for deletions to disable copying of deleted characters
-lvim.keys.normal_mode["d"] = '"_d'
-lvim.keys.normal_mode["D"] = '"_D'
-lvim.keys.normal_mode["c"] = '"_c'
-lvim.keys.normal_mode["C"] = '"_C'
-lvim.keys.normal_mode["x"] = '"_x'
-lvim.keys.visual_mode["d"] = '"_d'
-lvim.keys.visual_mode["D"] = '"_D'
-lvim.keys.visual_mode["c"] = '"_c'
-lvim.keys.visual_mode["C"] = '"_C'
-lvim.keys.visual_mode["x"] = '"_x'
+vim.api.nvim_set_keymap('n', 'd', '"_d', { noremap = true })
+vim.api.nvim_set_keymap('n', 'D', '"_D', { noremap = true })
+vim.api.nvim_set_keymap('n', 'c', '"_c', { noremap = true })
+vim.api.nvim_set_keymap('n', 'C', '"_C', { noremap = true })
+vim.api.nvim_set_keymap('n', 'x', '"_x', { noremap = true })
+vim.api.nvim_set_keymap('v', 'd', '"_d', { noremap = true })
+vim.api.nvim_set_keymap('v', 'D', '"_D', { noremap = true })
+vim.api.nvim_set_keymap('v', 'c', '"_c', { noremap = true })
+vim.api.nvim_set_keymap('v', 'C', '"_C', { noremap = true })
+vim.api.nvim_set_keymap('v', 'x', '"_x', { noremap = true })
