d8f9cabe12231ad12b63ae4d9fe716827b800df6
Hmz-x <hamzamusic34@gmail.com>
2025-02-02 19:25:49 +0300

    updated dl_sc.sh added proxychains.conf

--- misc/dl_sc.sh
+++ misc/dl_sc.sh
@@ -3,7 +3,25 @@
 DL_DIR="/home/hkm/Music/Leanmobb_Fukkst4r"
 USERNAME="lonestarfractal"
 
-[ ! -d "$DL_DIR" ] && mkdir -p "$DL_DIR"
+# Check if Tor service is running
+tor_status=$(systemctl is-active --quiet tor && echo "running" || echo "not running")
+# Check if proxychains4 executable is available
+proxychains_status=$(command -v proxychains4 >/dev/null && echo "installed" || echo "not installed")
 
-cd "$DL_DIR" && \
-  yt-dlp -f bestaudio "https://soundcloud.com/${USERNAME}/likes" &> /dev/null
+# If both conditions are met, execute the desired action
+if [[ "$tor_status" == "running" && "$proxychains_status" == "installed" ]]; then
+    echo "Both Tor is running and proxychains4 is installed."
+    echo "Downloading content..."
+else
+    echo "Either Tor is not running or proxychains4 is not installed. Exitting."
+    exit 1
+fi
+
+[ ! -d "$DL_DIR" ] && mkdir -p "$DL_DIR"
+cd "$DL_DIR" || { echo "Failed to cd into $DL_DIR. Exitting."; exit 1; }
+
+if [[ "$1" == "-q" || "$1" == "--quiet" ]]; then
+  proxychains4 -q yt-dlp -f bestaudio "https://soundcloud.com/${USERNAME}/likes" &> /dev/null
+else
+  proxychains4 -q yt-dlp -f bestaudio "https://soundcloud.com/${USERNAME}/likes"
+fi
